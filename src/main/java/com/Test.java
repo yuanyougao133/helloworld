6993a215999b58218b4cc58ea8d06d2fa046ac96
yuanyougao <yuanyougao@163.com>
2017-07-06 21:47:08 +0800

    提交

---
+++ src/main/java/com/Test.java
@@ -0,0 +1,14 @@
+package com;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello World");
+		
+		System.out.println("abcd");
+	}
+
+}
